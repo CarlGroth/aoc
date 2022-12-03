9a9128fefa8501b06aaab10702c4c9d6d4ac833b
Carl Groth <Callegroth@gmail.com>
2022-12-03 11:09:06 +0100

    Day 3

---
+++ 03/solution.py
@@ -0,0 +1,24 @@
+from functools import reduce
+
+with open('input.txt', 'r') as f:
+    arr = f.read().split('\n')
+
+
+def priority(letter):
+    return ord(letter) - 96 if letter.islower() else ord(letter) - 38
+
+
+def part1():
+    print(sum(priority((set(line[len(line)//2:]) & set(line[:len(line)//2])).pop()) for line in arr))
+
+
+def part2():
+    tot = 0
+    for i in range(0, len(arr), 3):
+        intersections = reduce(set.intersection, arr[i:i+3], set(arr[i]))
+        tot += priority(intersections.pop())
+    print(tot)
+
+
+part1()
+part2()
